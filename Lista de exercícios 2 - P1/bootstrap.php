bb91b27a8c9eb0fb3461dc1454176ef89b5be701
beatriztrombeta <beatriztrombeta@gmail.com>
2024-03-27 09:48:06 -0300

    fix: rotas post

--- Lista de exercícios 2 - P1/bootstrap.php
+++ Lista de exercícios 2 - P1/bootstrap.php
@@ -1,56 +1,94 @@
 <?php
 
-require __DIR__."/vendor/autoload.php";
+require __DIR__ . "/vendor/autoload.php";
 
 $metodo = $_SERVER['REQUEST_METHOD'];
 $caminho = $_SERVER['PATH_INFO'] ?? '/';
 
 $r = new Php\Listadeexercicios2\Router($metodo, $caminho);
 
-#ROTAS
+#ROTAS GET
 
-$r->get('/teste', function (){
-    return "teste";
-} );
-
-$r->get('/exercicio1', function(){
-    include"../public/exercises/exercicio1/exer1.php";
+$r->get('/exercicio1', function () {
+    include "../public/exercises/exercicio1/exer1.php";
 });
 
-$r->get('/exercicio2', function(){
-    include"../public/exercises/exercicio2/exer2.php";
+$r->get('/exercicio2', function () {
+    include "../public/exercises/exercicio2/exer2.php";
 });
 
-$r->get('/exercicio3', function(){
-    include"../public/exercises/exercicio3/exer3.php";
+$r->get('/exercicio3', function () {
+    include "../public/exercises/exercicio3/exer3.php";
 });
 
-$r->get('/exercicio4', function(){
-    include"../public/exercises/exercicio4/exer4.php";
+$r->get('/exercicio4', function () {
+    include "../public/exercises/exercicio4/exer4.php";
 });
 
-$r->get('/exercicio5', function(){
-    include"../public/exercises/exercicio5/exer5.php";
+$r->get('/exercicio5', function () {
+    include "../public/exercises/exercicio5/exer5.php";
 });
 
-$r->get('/exercicio6', function(){
-    include"../public/exercises/exercicio6/exer6.php";
+$r->get('/exercicio6', function () {
+    include "../public/exercises/exercicio6/exer6.php";
 });
 
-$r->get('/exercicio7', function(){
-    include"../public/exercises/exercicio7/exer7.php";
+$r->get('/exercicio7', function () {
+    include "../public/exercises/exercicio7/exer7.php";
 });
 
-$r->get('/exercicio8', function(){
-    include"../public/exercises/exercicio8/exer8.php";
+$r->get('/exercicio8', function () {
+    include "../public/exercises/exercicio8/exer8.php";
 });
 
-$r->get('/exercicio9', function(){
-    include"../public/exercises/exercicio9/exer9.php";
+$r->get('/exercicio9', function () {
+    include "../public/exercises/exercicio9/exer9.php";
 });
 
-$r->get('/exercicio10', function(){
-    include"../public/exercises/exercicio10/exer10.php";
+$r->get('/exercicio10', function () {
+    include "../public/exercises/exercicio10/exer10.php";
+});
+
+#ROTAS POST
+
+$r->post('/exercicio1', function () {
+    include "../public/exercises/exercicio1/exer1.php";
+});
+
+$r->post('/exercicio2', function () {
+    include "../public/exercises/exercicio2/exer2.php";
+});
+
+$r->post('/exercicio3', function () {
+    include "../public/exercises/exercicio3/exer3.php";
+});
+
+$r->post('/exercicio4', function () {
+    include "../public/exercises/exercicio4/exer4.php";
+});
+
+$r->post('/exercicio5', function () {
+    include "../public/exercises/exercicio5/exer5.php";
+});
+
+$r->post('/exercicio6', function () {
+    include "../public/exercises/exercicio6/exer6.php";
+});
+
+$r->post('/exercicio7', function () {
+    include "../public/exercises/exercicio7/exer7.php";
+});
+
+$r->post('/exercicio8', function () {
+    include "../public/exercises/exercicio8/exer8.php";
+});
+
+$r->post('/exercicio9', function () {
+    include "../public/exercises/exercicio9/exer9.php";
+});
+
+$r->post('/exercicio10', function () {
+    include "../public/exercises/exercicio10/exer10.php";
 });
 
 
@@ -58,7 +96,7 @@ $r->get('/exercicio10', function(){
 
 $resultado = $r->handler();
 
-if(!$resultado){
+if (!$resultado) {
     http_response_code(404);
     echo "Página não encontrada!";
     die();
