a31dbe7f29e8b9fc7edb3cef9cc4f85402279554
beatriztrombeta <beatriztrombeta@gmail.com>
2024-04-12 20:28:48 -0300

    feat: estrutura public

---
+++ Projeto P1/public/index.php
@@ -0,0 +1,10 @@
+<?php 
+
+//Use "namespace Php\Projetop1;"
+
+//criar tabelas no mysql
+//criar classes da model
+//criar classe dao com o metodo inserir e com as funcoes do pdo
+//definir as rotas dentro do bootstrap
+//criar classes controller
+//criar as views
