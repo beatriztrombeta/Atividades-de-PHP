cb8936e79f3de686c68e3909ccf5d565770f2e40
Beatriz Trombeta <beatriztrombeta@gmail.com>
2024-04-13 16:45:21 -0300

    feat: include __DIR__

--- Projeto P1/public/index.php
+++ Projeto P1/public/index.php
@@ -1,10 +1,9 @@
-<?php 
+<?php
+    include __DIR__.'/../bootstrap.php';
 
-//Use "namespace Php\Projetop1;"
-
-//criar tabelas no mysql
-//criar classes da model
-//criar classe dao com o metodo inserir e com as funcoes do pdo
-//definir as rotas dentro do bootstrap
+//criar tabelas no mysql - FEITO
+//criar classes da model - FEITO
+//criar classe dao com o metodo inserir e com as funcoes do pdo - FEITO
+//definir as rotas dentro do bootstrap - FEITO
 //criar classes controller
 //criar as views
