581bf6b3ecf073252da57f567c7ed9c7c616826a
Beatriz Trombeta <beatriztrombeta@gmail.com>
2024-04-17 16:44:07 -0300

    fix: remoção de comentários

--- Projeto P1/public/index.php
+++ Projeto P1/public/index.php
@@ -1,9 +1,2 @@
 <?php
     include __DIR__.'/../bootstrap.php';
-
-//criar tabelas no mysql - FEITO
-//criar classes da model - FEITO
-//criar classe dao com o metodo inserir e com as funcoes do pdo - FEITO
-//definir as rotas dentro do bootstrap - FEITO
-//criar classes controller
-//criar as views
